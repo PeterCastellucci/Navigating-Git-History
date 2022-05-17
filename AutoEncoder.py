c5a3da08ff994002cb2db75b331b5943c2aa244e
Peter <peter.castellucci@postgrad.manchester.ac.uk>
2022-05-17 14:30:17 +0100

    IMPORT MODULES

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -1,1 +1,14 @@
-ls
+# -*- coding: utf-8 -*-
+"""
+Generate synthetic low dimensional manifold in a 6D space.
+
+Task for students: Extend the script to find the manifold
+using an auto-encoder. See assignment details on Blackboard
+"""
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import torch.optim as optim
+import time
