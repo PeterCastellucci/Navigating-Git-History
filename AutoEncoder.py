8fe6c1536ff930dd4bbf24196a6b1b4ccc49e8fe
Peter <peter.castellucci@postgrad.manchester.ac.uk>
2022-05-17 14:33:48 +0100

    ADD FUNCTION TO MAKE MANIFOLD SAMPLES

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -12,3 +12,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import torch.optim as optim
 import time
+
+student_id=10050985
+
+np.random.seed(student_id)
+
+n_samples=300
+
+def make_manifold_samples(n_samples):
+    """Generate samples in a low dimensional space"""
+    X=np.zeros([n_samples,5])
+    X[:,0]=np.arange(-2,2,4/n_samples)
+    X[:,1]=np.random.random(n_samples)-0.5
+    if (student_id%2==0):
+        X[:,2]=np.random.random(n_samples)-0.5
+    X[:,3]=np.sin(4*X[:,0])
+    if (student_id%3==0):
+        X[:,4]=X[:,0]**2
+    return X
